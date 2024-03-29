5cdc30eb9a55b16b3058337ec47ad92846af0898
SyntaxBugg <zarbotbekovabbosbek@gmail.com>
2024-03-30 01:42:45 +0500

    changes sqlite

--- NEWS_MAIN/urls.py
+++ NEWS_MAIN/urls.py
@@ -49,5 +49,6 @@ urlpatterns = [
                   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(
+    gs.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
